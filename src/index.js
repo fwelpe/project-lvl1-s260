e555f15364faa50076c639b2e4e29afb2bf4dc71
fwlpe <arestovfa@gmail.com>
2018-04-18 23:04:38 +0300

    stage 3 v7

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import readlineSync from 'readline-sync';
 export default () => {
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
-  return username;
+   return username;
 };
