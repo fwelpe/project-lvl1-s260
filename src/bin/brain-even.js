d3991d61989c4dcae666ee4cf62d7e479a873e32
fwlpe <arestovfa@gmail.com>
2018-04-19 16:42:25 +0300

    stage 4 v1 rdy to check

---
+++ src/bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import * as code from '..';
+
+code.greetings();
+code.brainevenRules();
+const name = code.getName();
+code.firstGame(name);
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import getName from '..';
+import { greetings, getName } from '..';
 
-console.log('Welcome to the Brain Games!');
+greetings();
 getName();
